3d4394ab54d322a9f4951261fe612d4f81b559cb
João Henrique Machado Silva <joao.henrique-silva@dazn.com>
2019-06-28 16:02:23 +0200

    fix some funcionality issues; now serving file

--- server.c
+++ server.c
@@ -109,50 +109,55 @@ void startServer(char *port){
 }
 
 // client connection
-void respond(int n){
-    char mesg[99999], *reqline[3], data_to_send[BYTES], path[99999];
-    int rcvd, fd, bytes_read;
+void respond(int n)
+{
+	char mesg[99999], *reqline[3], data_to_send[BYTES], path[99999];
+	int rcvd, fd, bytes_read;
 
-    memset((void*)mesg, (int)'\0', 99999);
-    rcvd = recv(clients[n], mesg, 99999, 0);
+	memset( (void*)mesg, (int)'\0', 99999 );
 
-    if (rcvd < 0) {
-        fprintf(stderr, ("recv() error\n"));
-    }else if (rcvd == 0) {
-        fprintf(stderr, "Client disconnected unexpectedly.\n");
-    }else {
-        printf("%s\n", mesg);
-        reqline[0] = strtok(mesg, "\t\n");
-        if ( strncmp(reqline[0], "GET\0", 4)==0 )
+	rcvd=recv(clients[n], mesg, 99999, 0);
+
+	if (rcvd<0)    // receive error
+		fprintf(stderr,("recv() error\n"));
+	else if (rcvd==0)    // receive socket closed
+		fprintf(stderr,"Client disconnected upexpectedly.\n");
+	else    // message received
+	{
+		printf("%s", mesg);
+		reqline[0] = strtok (mesg, " \t\n");
+		if ( strncmp(reqline[0], "GET\0", 4)==0 )
 		{
 			reqline[1] = strtok (NULL, " \t");
 			reqline[2] = strtok (NULL, " \t\n");
-            if (strncmp(reqline[0], "HTTP/1.0", 8) != 0 && strncmp(reqline[2], "HTTP/1.1", 8) != 0) {
-                write(clients[n], "HTTP1.0 400 Bad Request\n", 25);
-            }else {
-                if (strncmp(reqline[1], "/\0", 2) == 0) {
-                    reqline[1] = "/index.html"; 
-                }
-                strcpy(path, ROOT);
-                strcpy(&path[strlen(ROOT)], reqline[1]);
-                printf("file: %s\n", path);
+			if ( strncmp( reqline[2], "HTTP/1.0", 8)!=0 && strncmp( reqline[2], "HTTP/1.1", 8)!=0 )
+			{
+				write(clients[n], "HTTP/1.0 400 Bad Request\n", 25);
+			}
+			else
+			{
+				if ( strncmp(reqline[1], "/\0", 2)==0 )
+					reqline[1] = "/index.html";        //Because if no file is specified, index.html will be opened by default (like it happens in APACHE...
 
-                if ((fd = open(path, O_RDONLY)) != -1) {
-                    send(clients[n], "HTTP/1.0 200 OK\n\n", 17, 0);
-                    while((bytes_read = read(fd, data_to_send, bytes_read)) > 0) {
-                        write(clients[n], data_to_send, bytes_read);
-                    }
-                }else{
-                    write(clients[n], "HTTP/1.0 404 Not Found\n", 23);
-                }
-            }
-        }
-    }
+				strcpy(path, ROOT);
+				strcpy(&path[strlen(ROOT)], reqline[1]);
+				printf("file: %s\n", path);
 
-    //Closing SOCKET
-    shutdown(clients[n], SHUT_RDWR);
-    close(clients[n]);
-    clients[n] =- 1;
+				if ( (fd=open(path, O_RDONLY))!=-1 )    //FILE FOUND
+				{
+					send(clients[n], "HTTP/1.0 200 OK\n\n", 17, 0);
+					while ( (bytes_read=read(fd, data_to_send, BYTES))>0 )
+						write (clients[n], data_to_send, bytes_read);
+				}
+				else    write(clients[n], "HTTP/1.0 404 Not Found\n", 23); //FILE NOT FOUND
+			}
+		}
+	}
+
+	//Closing SOCKET
+	shutdown (clients[n], SHUT_RDWR);         //All further send and recieve operations are DISABLED...
+	close(clients[n]);
+	clients[n]=-1;
 }
 
 void error(char *e){
